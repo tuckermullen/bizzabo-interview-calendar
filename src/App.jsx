6c10488593b09d5b6efd67678f26493e4677aa7d
Tucker M <mullentucker@gmail.com>
2020-11-16 10:56:39 -0800

    Clean up App.jsx, move components to their own file, use spread operator for props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import CalendarModal from './components/CalendarModal';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import { Modal, Table, Image } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
 import axios from 'axios'
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 moment.locale('en-GB');
 const localizer = momentLocalizer(moment)
@@ -87,32 +88,18 @@ class App extends Component {
   }
 
   render() {
-
-    const { bizzaboEvents,
-            modalIsOpen,
-            eventImage,
-            eventName,
-            eventID,
-            eventType,
-            eventStartDate,
-            eventEndDate,
-            eventTimezone,
-            eventLocation,
-            eventWebsite,
-            eventSupportEmail,
-            eventStatus
-          } = this.state
+    let self = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
-          <Image className="logo" src="BizzaboLogo.png" rounded />
+          <Image className="logo" src="BizzaboLogo.png" alt="Bizzabo Logo" rounded />
           <h2 className="logo-header">Scheduled Events</h2>
         </header>
         <div className="Calendar-div" style={{ height: 700 }}>
           <Calendar
             localizer={localizer}
-            events={bizzaboEvents}
+            events={self.bizzaboEvents}
             selectable={true}
             onSelectEvent={this.handleOpen}
             popup={true}
@@ -121,60 +108,11 @@ class App extends Component {
             views={['month','week','day']}
             defaultDate={new Date()}
           />
-          <Modal
-            show={modalIsOpen}
-            onHide={this.handleClose}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            autoFocus={true}
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>
-                <strong>{eventName}</strong> - Event ID: {eventID}<br/>
-                <Image src={eventImage} rounded />
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Table striped bordered hover size="xl" responsive="lg">
-                <thead>
-                  <tr>
-                    <th>Type</th>
-                    <th>Start Time/Date</th>
-                    <th>End Time/Date</th>
-                    <th>Timezone</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{eventType}</td>
-                    <td>{eventStartDate}</td>
-                    <td>{eventEndDate}</td>
-                    <td>{eventTimezone}</td>
-                  </tr>
-                </tbody>
-              </Table>
-              <Table striped bordered hover size="xl" responsive="lg">
-                <thead>
-                  <tr>
-                    <th>Location</th>
-                    <th>Event Website</th>
-                    <th>Support Email</th>
-                    <th>Event Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{eventLocation}</td>
-                    <td>{eventWebsite}</td>
-                    <a href={ `mailto:${eventSupportEmail}` }><td>{eventSupportEmail}</td></a>
-                    <td>{eventStatus}</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Modal.Body>
-          </Modal>
         </div>
+        <CalendarModal
+          handleClose={this.handleClose}
+          {...self}
+        />
       </div>
     );
   }
